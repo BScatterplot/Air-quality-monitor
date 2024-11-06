8b63920997a9362c0de634f53c5f46df4d3c4705
Bert <89002470+BScatterplot@users.noreply.github.com>
2024-11-05 21:29:59 -0600

    Added Wifi and MQTT functionality, tested and seems to be working.

--- src/air-quality-monitor/air-quality-monitor.ino
+++ src/air-quality-monitor/air-quality-monitor.ino
@@ -22,10 +22,31 @@ BME280 needs to have SDO grounded, resulting in an address of 0x76.
 #include <pas-co2-ino.hpp>
 #define XEN_I2C_FREQ_HZ  100000  
 
+//Wifi
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#define MSG_BUFFER_SIZE (500)
+#include "secret.h"
+
+//const char* ssid = "******";
+//const char* wifi_pw = "******";
+//const char* mqtt_server = "******";
+//const char* clientId = "******";
+//const char* mqtt_user = "******";
+//const char* mqtt_pw = "******";
+
+const char* outTopic_info = "airquality/info";
+const char* outTopic_data = "airquality/data";
+
 Adafruit_BME280 bme;    //BME280 object
 PASCO2Ino co2_sensor;        //PASCO2 object
 
-unsigned long meas_period = 5000;       //Measurement period in ms
+WiFiClient espClient;
+PubSubClient client(espClient);
+unsigned long lastMsg = 0;
+char mqttOutMsg[MSG_BUFFER_SIZE];
+
+unsigned long meas_period = 60000;       //Measurement period in ms
 Error_t co2_err;
 
 void setup() 
@@ -60,44 +81,105 @@ void setup()
       Serial.println(co2_err);
       while (1) delay(10);
     }
+    //wifi
+    setup_wifi();
+    client.setServer(mqtt_server, 1883);
+    delay(meas_period);
 }
 
 void loop()
 {
-  delay(meas_period);
+  client.loop();
 
-  float curCo2, curTempC, curPressureHPa, curHumidity;
-  int16_t co2ppm;
-
-  //Read BME280 first since it feeds into the CO2 sensor
-  curTempC = bme.readTemperature();
-  curPressureHPa = (bme.readPressure() / 100.0F);
-  curHumidity = bme.readHumidity();
-
-  Serial.printf("Temperature: %.1f °C\n",curTempC);
-  Serial.printf("Pressure: %.1f hPa\n", curPressureHPa);
-  Serial.printf("Humidity: %.1f\n",curHumidity);
-  
-  co2_err = co2_sensor.getCO2(co2ppm);
-  if (co2_err != XENSIV_PASCO2_OK)
-  {  
-    if (co2_err == XENSIV_PASCO2_ERR_COMM);
-    {  delay(600);
-       co2_err = co2_sensor.getCO2(co2ppm);
-       if (co2_err != XENSIV_PASCO2_OK)
-       {
-         Serial.print("Get CO2 error: ");
-         Serial.println(co2_err);
-       }
-    }
-  }
-  Serial.printf("CO2 level: %d ppm\n",co2ppm);
-
-  //Example code sets the pressure reference, delays, then reads the value.
-  co2_err = co2_sensor.setPressRef(curPressureHPa);
-  if (co2_err != XENSIV_PASCO2_OK)
+  if ((millis() - lastMsg) > meas_period)
   {
-    Serial.print("Pressure reference error: ");
-    Serial.println(co2_err);
+    lastMsg = millis();
+  
+    if (!client.connected())
+    {
+      reconnect();
+    }
+
+
+    float curCo2, curTempC, curPressureHPa, curHumidity;
+    int16_t co2ppm;
+
+    //Read BME280 first since it feeds into the CO2 sensor
+    curTempC = bme.readTemperature();
+    curPressureHPa = (bme.readPressure() / 100.0F);
+    curHumidity = bme.readHumidity();
+
+    Serial.printf("Temperature: %.1f °C\n",curTempC);
+    Serial.printf("Pressure: %.1f hPa\n", curPressureHPa);
+    Serial.printf("Humidity: %.1f\n",curHumidity);
+    
+    co2_err = co2_sensor.getCO2(co2ppm);
+    if (co2_err != XENSIV_PASCO2_OK)
+    {  
+      if (co2_err == XENSIV_PASCO2_ERR_COMM);
+      {  delay(600);
+        co2_err = co2_sensor.getCO2(co2ppm);
+        if (co2_err != XENSIV_PASCO2_OK)
+        {
+          Serial.print("Get CO2 error: ");
+          Serial.println(co2_err);
+        }
+      }
+    }
+    Serial.printf("CO2 level: %d ppm\n",co2ppm);
+
+    //Example code sets the pressure reference, delays, then reads the value.
+    co2_err = co2_sensor.setPressRef(curPressureHPa);
+    if (co2_err != XENSIV_PASCO2_OK)
+    {
+      Serial.print("Pressure reference error: ");
+      Serial.println(co2_err);
+    }
+
+    format_msg(mqttOutMsg, curTempC, curPressureHPa, curHumidity, co2ppm);
+    client.publish(outTopic_data, mqttOutMsg);
+    Serial.println(mqttOutMsg);
   }
+}
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Attempting Wifi connection to ");
+  Serial.println(ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, wifi_pw);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void reconnect()
+{
+  Serial.print("Attempting MQTT connection...");
+  
+  // Attempt to connect
+  if (client.connect(clientId, mqtt_user, mqtt_pw)) {
+    Serial.println("connected");
+    // Once connected, publish an announcement...
+    client.publish(outTopic_info, "Connected");
+  } else {
+    Serial.print("failed, rc= ");
+    Serial.println(client.state());
+  }
+}
+
+void format_msg(char* buffer, float tempC, float pressureHPa, float humidity, unsigned int lvlCO2)
+{
+  snprintf(buffer, MSG_BUFFER_SIZE, "{\n\"TempC\":%.1f,\n\"Humidity\":%.1f,\n\"CO2\":%d,\n\"Pressure\":%.1f\n}", tempC, humidity, lvlCO2, pressureHPa);
 }
